0dc9db7f850c8ffa076a5fbb22fae8eef6c186cc
gurgelt <paulo.gtcanal@gmail.com>
2021-08-31 19:19:38 -0300

    first commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		//Uma sentença de código termina com ;
+		//Mais um comentário...
+		//Fim
+		System.out.println("Primeiro Programa Parte #1!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3...
+		 */
+		System.out.println("Primeiro Programa Parte #2!");
+	}	
+}
+ 
